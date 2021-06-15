7836698f187da41710ac7fe0764bf8299cd10231
MAD71241 <michele.deliso@gmail.com>
2021-06-15 18:30:51 +0200

    creo struttura dati per card dinamiche

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = new Vue ({
 
     data: {
         searchQuery: "",
+        coursesImgSize: "480x298",
+        coursesImgExt: ".jpg",
         navbarLinks: [
             "Home",
             "Pages",
@@ -15,6 +17,52 @@ const app = new Vue ({
         ,
         logos: [
             "client-logo-01", "client-logo-02", "client-logo-03", "client-logo-04", "client-logo-05", "client-logo-06", 
+        ]
+        ,
+        courses: [
+            {
+                price: 40.00,
+                title: "Learning to Write as a Professional Author",
+                lessons: 20,
+                students: 50,
+                imgpath: "stock-full-hd-01-"
+            }
+            ,            {
+                price: "Free",
+                title: "Customer-centric Info-Tech Strategies",
+                lessons: 24,
+                students: 769,
+                imgpath: "stock-full-hd-02-"
+            }
+            ,            {
+                price: 19.00,
+                title: "Open Programming Courses for Everyone: Python",
+                lessons: 17,
+                students: 62,
+                imgpath: "stock-full-hd-03-"
+            }
+            ,            {
+                price: 26.00,
+                title: "Academic Listening and Note-taking",
+                lessons: 14,
+                students: 67,
+                imgpath: "stock-full-hd-04-"
+            }
+            ,            {
+                price: 39.00,
+                title: "Master JQuery in a Short Period of Time",
+                lessons: 6,
+                students: 51,
+                imgpath: "stock-full-hd-05-"
+            }
+            ,            {
+                price: 59.00,
+                title: "Introduction to Javascript for Beginners",
+                lessons: 14,
+                students: 76,
+                imgpath: "stock-full-hd-06-"
+            }
+            ,
         ]
     }
     ,
