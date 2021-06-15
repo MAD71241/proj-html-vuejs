1c20f1433610539404ed23aa5ee12629786596a9
MAD71241 <michele.deliso@gmail.com>
2021-06-15 19:17:50 +0200

    completo sezione courses

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const app = new Vue ({
 
     data: {
         searchQuery: "",
-        coursesImgSize: "480x298",
-        coursesImgExt: ".jpg",
         navbarLinks: [
             "Home",
             "Pages",
@@ -25,42 +23,42 @@ const app = new Vue ({
                 title: "Learning to Write as a Professional Author",
                 lessons: 20,
                 students: 50,
-                imgpath: "stock-full-hd-01-"
+                imgpath: "stock-full-hd-01-480x298.jpg"
             }
             ,            {
-                price: "Free",
+                price: 0,
                 title: "Customer-centric Info-Tech Strategies",
                 lessons: 24,
                 students: 769,
-                imgpath: "stock-full-hd-02-"
+                imgpath: "stock-full-hd-02-480x298.jpg"
             }
             ,            {
                 price: 19.00,
                 title: "Open Programming Courses for Everyone: Python",
                 lessons: 17,
                 students: 62,
-                imgpath: "stock-full-hd-03-"
+                imgpath: "stock-full-hd-03-480x298.jpg"
             }
             ,            {
                 price: 26.00,
                 title: "Academic Listening and Note-taking",
                 lessons: 14,
                 students: 67,
-                imgpath: "stock-full-hd-04-"
+                imgpath: "stock-full-hd-04-480x298.jpg"
             }
             ,            {
                 price: 39.00,
                 title: "Master JQuery in a Short Period of Time",
                 lessons: 6,
                 students: 51,
-                imgpath: "stock-full-hd-05-"
+                imgpath: "stock-full-hd-05-480x298.jpg"
             }
             ,            {
                 price: 59.00,
                 title: "Introduction to Javascript for Beginners",
                 lessons: 14,
                 students: 76,
-                imgpath: "stock-full-hd-06-"
+                imgpath: "stock-full-hd-06-480x298.jpg"
             }
             ,
         ]
